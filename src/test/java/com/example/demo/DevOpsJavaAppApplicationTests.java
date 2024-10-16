02bdd84dfc6b25465ecf91044ed7e03e5ecdb9e3
JEEVA ABISHAKE <jeevaabishake2002@gmail.com>
2024-10-16 16:32:17 +0530

    Update DevOpsJavaAppApplicationTests.java

--- src/test/java/com/example/demo/DevOpsJavaAppApplicationTests.java
+++ src/test/java/com/example/demo/DevOpsJavaAppApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DevOpsJavaAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void helloEndpointTest() {
+        // Test the /hello endpoint
+        ResponseEntity<String> response = restTemplate.getForEntity("/hello", String.class);
+        assertThat(response.getBody()).isEqualTo("Hello, DevOps World from Java!");
+    }
 }
